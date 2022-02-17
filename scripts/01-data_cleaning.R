464d9d674d051d86ac0fe282573b2001e8565b5f
Sid Gupta <sid.gupta@mail.utoronto.ca>
2022-02-16 21:06:54 -0500

    Adding scripts to extract column names

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -23,6 +23,12 @@ library(tidyr)
 library(janitor)
 library(dplyr)
 
+
+# For the BRFSS dataset, the questionnaire data is:
+# https://www.cdc.gov/brfss/questionnaires/pdf-ques/2012_BRFSS.pdf
+# Use CTRL+F To search for information about mammography columns
+# More information about BRFSS is available: https://www.cdc.gov/brfss/annual_data/annual_2012.html
+# BRFSS columns are: https://www.cdc.gov/brfss/annual_data/2012/LLCP_VarLayout_12_OneColumn.HTML
 brfss_2008 <- read_xpt(
   here::here("inputs/data/CDBRFS08.XPT"),
   skip = 0,
